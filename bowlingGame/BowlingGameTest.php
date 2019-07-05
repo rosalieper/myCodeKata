f380ab48d203e60a068fde2c2b962ea251817e55
rosalie <perside.rosalie@wikimedia.de>
2019-07-05 15:06:49 +0200

    Refactoring BowlingGameTest file

--- bowlingGame/BowlingGameTest.php
+++ bowlingGame/BowlingGameTest.php
@@ -16,20 +16,25 @@ class BowlingGameTest extends PHPUnit\Framework\TestCase {
         $this->g = new Game();
     }
 
+    public function rollMany($totalNumRoll, $numPinsDown ){
+        for( $i = 0; $i < $totalNumRoll; $i++ ) {
+            $this->g->roll($numPinsDown);
+        }
+    }
+
     /**
      * @throws Exception
      */
     public function testGutterGame() {
-        for( $i = 0; $i < 20; $i++ ) {
-            $this->g->roll(0);
-        }
+        $this->rollMany(20, 0);
         $this->assertEquals( 0, $this->g->score() );
     }
 
+    /**
+     * @throws Exception
+     */
     public function testAllOnes() {
-        for( $i = 0; $i < 20; $i++ ) {
-           $this->g->roll(1);
-        }
+        $this->rollMany(20, 1);
         $this->assertEquals( 20, $this->g->score() );
     }
 }
