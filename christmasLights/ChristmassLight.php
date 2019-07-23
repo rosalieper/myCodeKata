faf1c73ed814c15eff9842f8874650bcb9a8c2c3
rosalie <perside.rosalie@wikimedia.de>
2019-07-23 10:43:14 +0200

    Testing pre-commit hook

---
+++ christmasLights/ChristmassLight.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class ChristmassLight {
+
+    public function printing() {
+        $text = 'hello';
+        print_r('holla');
+        echo 'hoollaa';
+        var_dump( $text );
+    }
+}
+
+$x = new ChristmassLight();
+ $x->printing();
---
+++ christmasLights/ChristmassLightTest.php
@@ -0,0 +1,10 @@
+<?php
+
+
+class ChristmassLightTest extends \PHPUnit\Framework\TestCase {
+
+    public function testPrinting() {
+        print_r('hello');
+    }
+
+}
